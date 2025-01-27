de1ed3ebbed70088350bbb7ad08dcf3a58a4c94b
vknir <kutkarsh.umar@gmail.com>
2025-01-27 19:36:22 +0530

    remove /dist

--- backend/dist/config/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGO_URL = void 0;
-require("dotenv/config");
-const MONGO_URL = process.env.MONGO_URL || ""; // default empty string to avoid type error
-exports.MONGO_URL = MONGO_URL;
